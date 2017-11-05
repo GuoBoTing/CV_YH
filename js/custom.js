f467d60c12f6680a44a44d74214b13590b527f5b
guoboting <guoboting@gmail.com>
2017-11-05 23:14:38 +0800

    ninit

---
+++ js/custom.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+	// header color change
+	$(window).on("scroll", function(){
+		var scrollDistance = $(window).scrollTop();
+		var $header = $(".js-header");
+		if (scrollDistance > 80) { 
+			$header.addClass("header-colored") 
+		}
+		else {
+			$header.removeClass("header-colored")
+		}
+	})
+
+	// initialize plugin WOW
+	if (screen.width > 768) {
+		new WOW().init();
+	}
+	
+	// finctbox plugin option
+	$("[data-fancybox]").fancybox({
+		loop:true
+	});
+})
